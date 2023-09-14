b8efb6daf4688dd4cf51e3bced79878ad5fb0024
gabimarin <gabriela.santos9@estudante.ifms.edu.br>
2023-09-14 07:11:17 -0400

    dddd

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,67 +1,62 @@
 import { useState } from 'react'
+
 import Filme from './../filme/Filme'
 import './Main.css'
-
-//import lupa from '../img/lupa.png';
+import lupa from '../img/lupa.png';
 
 type FilmeType = {
-    id:number,
-    titulo:string,
-    sinopse:string,
-    imagem:string
+       id:number,
+       titulo:string,
+       sinopse:string,
+       imagem:string
 }
 
 export default function Main() {
-    //let textodigitado = 'Barbie'
-    //Hooks são funções do React que ajudam a gente a fazer tarefas
-    //específicas
-    const [texto,setTexto]=useState("")
+    const [texto, setTexto]=useState("")
 
     const filmes:FilmeType[] = [
-        {
+       {
               id: 1,
               titulo:'barbie',
               sinopse:"atatata",
               imagem: "/barbie.png"
-        },
-        {
+       },
+       {
               id: 2,
               titulo:'quebranozes',
               sinopse:"blublublu",
               imagem: "/quebranozes.webp"
-        },
-        {
+       },
+       {
               id: 3,
               titulo:'lagocisnes',
               sinopse:"iiiiiii",
-              imagem: "/lagodoscisnes.jpg"
-        }
+              imagem: "/lagodoscisner.jpg"
+       }
     ]
-
-    //O parâmetro "e" da minha função será o meu evento que ocorreu
+    
     function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
-        //console.log(e.target.value)
-        //Como eu faço para mudar o texto para "TERE"
-        setTexto(e.target.value)
+        console.log(e.target.value)
+       setTexto(e.target.value)
     }
-    return (
-        <>
-            <div className="campo_pesquisa">
-                <p>Busque um filme</p>
-                <input type="text" 
-                       className='botao_pesquisa'
-                       placeholder='Pesquise um Filme'
-                       onChange={TrataTexto} />
-                {texto && <p>Resultados Para: {texto} </p>}
-            </div>
-            <main className="content-main">
-                {/* 
-                    Use algo do vetor para tentar criar os filmes 
-                */}
+
+    return(
+<>
+        {
+              <div className="campopesquisa"> 
+                     <div id="divBusca">
+                            <input type="text" className="botao_pesquisa" placeholder="Encontre o filme desejado..." onChange={TrataTexto}/>
+                            <button className="divBusca"><img src={lupa}></img></button>
+                    </div>
+                    {(texto)?<p>Resultados para: {texto}</p>:""}
+              </div>
+       }
+       
+        <main className="content-main">
 
 
-                
-                {/* <Filme titulo="Barbie Live Action" 
+             {/*n tem essa chave  
+             <Filme titulo="Barbie Live Action" 
                     sinopse="Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, 
                     Barbie parte para o mundo humano em busca da verdadeira felicidade."
                     imagem= "/barbie.png"/>
@@ -95,11 +90,8 @@ export default function Main() {
                     sinopse="Genevieve e suas irmãs adoram dançar. Como são impedidas pela Duquesa Rowena, as princesas bailarinas descobrem um mundo mágico onde dançam sem serem 
                     incomodadas. Porém, acontecimentos mais urgentes as levam a tentar salvar a vida do pai."
                     imagem= "/dozeprincesas.png"/> */}
+        </main>
 
-            </main>
         </>
     )
 }
-
-
-
