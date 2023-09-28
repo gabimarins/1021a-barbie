02687404e0a32c2cbd43d1b3022714f056005179
gabimarin <gabriela.santos9@estudante.ifms.edu.br>
2023-09-28 08:21:58 -0400

    aleaTORIO

--- src/componentes/link/Link.tsx
+++ src/componentes/link/Link.tsx
@@ -1,6 +1,14 @@
-function Link(props:{texto:string}){
+import {Link as LinkNav}  from 'react-router-dom'
+
+type LinkProps = {
+    url:string,
+    texto:string
+}
+
+function Link(props:LinkProps){
     return(
-        <a href="#">{props.texto}</a>
+        <LinkNav to={props.url}>{props.texto}</LinkNav>
+        //<a href={props.url}>{props.texto}</a>
     )
 }
 export default Link
